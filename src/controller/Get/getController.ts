d98aa130825d21e64501d1c287e225dbbe0d0ccc
tharun-2511 <dharunkumar2511@gmail.com>
2022-11-22 09:16:37 +0530

    file changed to bug reporting

--- src/controller/Get/getController.ts
+++ src/controller/Get/getController.ts
@@ -1,14 +1,14 @@
-import { Request,Response } from "express";
-import getservice from '../../service/getServices'
-import 
+// import { Request,Response } from "express";
+// import getservice from '../../service/getServices'
+// import 
 
 
 
-export default class GetController{
-    public async getlist(req:Request,res:Response) {
-        try{
-            let getcontroller= new GetController()
+// export default class GetController{
+//     public async getlist(req:Request,res:Response) {
+//         try{
+//             let getcontroller= new GetController()
 
-        }
-    }
-}
+//         }
+//     }
+// }
--- src/modules/get/model.ts
+++ src/modules/get/model.ts
@@ -1,1 +1,1 @@
-export interface Iget
+// export interface Iget
